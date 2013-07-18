4a0e82d82bc3488ae380ffe12bcf05854381048b
Alexander Saltanov <asd@mokote.com>
2013-07-18 23:06:41 +0400

    Add a test for copying permission bits. Unify quotation style.

--- tests.py
+++ tests.py
@@ -6,15 +6,17 @@ import unittest
 from atomicfile import AtomicFile
 
 
-def create_test_file(filename):
-    f = open(filename, 'wb')
-    f.write(b'test\n')
+def create_test_file(filename, content=b'test\n', mode=None):
+    f = open(filename, "wb")
+    f.write(content)
     f.close()
+    if mode is not None:
+        os.chmod(filename, mode)
 
 
 class AtomicFileTest(unittest.TestCase):
     def setUp(self):
-        self.filename = 'test-atomicfile.txt'
+        self.filename = "test-atomicfile.txt"
 
     def test_write(self):
         create_test_file(self.filename)
@@ -23,7 +25,7 @@ class AtomicFileTest(unittest.TestCase):
         af.write(expected)
         af.close()
 
-        f = open(self.filename, 'rb')
+        f = open(self.filename, "rb")
         result = f.read()
         f.close()
         try:
@@ -33,10 +35,10 @@ class AtomicFileTest(unittest.TestCase):
 
     def test_close(self):
         af = AtomicFile(self.filename)
-        af.write(b'test\n')
+        af.write(b"test\n")
         af.close()
         try:
-            af.write(b'test again\n')
+            af.write(b"test again\n")
             self.fail('ValueError not raised')
         except ValueError:
             pass
@@ -57,6 +59,20 @@ class AtomicFileTest(unittest.TestCase):
         finally:
             os.remove(self.filename)
 
+    def test_permissions(self):
+        expected_mode = 0o741
+        create_test_file(self.filename, mode=expected_mode)
 
-if __name__ == '__main__':
+        af = AtomicFile(self.filename)
+        af.write(b"I don't really care of the content.\n")
+        af.close()
+
+        st_mode = os.lstat(self.filename).st_mode & 0o777
+        try:
+            self.assertEqual(st_mode, expected_mode)
+        finally:
+            os.remove(self.filename)
+
+
+if __name__ == "__main__":
     unittest.main()
