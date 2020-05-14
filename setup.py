99a196492486895b1a1c442ec77f5dea4363f9e6
Alexander <asd@mokote.com>
2020-05-14 23:30:59 +0300

    Update homepage in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@
 
 from setuptools import setup
 
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='atomicfile',
-    version='1.0',
+    version='1.0.1',
     author='Alexander Saltanov',
     author_email='asd@mokote.com',
-    url='http://github.com/sashka/atomicfile',
+    url='http://github.com/sashka/atomicfile-py',
     license='MIT',
     description='Writeable file object that atomically updates a file.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     py_modules=['atomicfile'],
     test_suite='tests',
     classifiers=[
